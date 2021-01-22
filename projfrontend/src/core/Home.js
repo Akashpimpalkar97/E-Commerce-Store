9efd3b39789d70ff796203fa8c50ad1df52273be
Akash Pimpalkar <akashpimpalkar97@gmail.com>
2021-01-22 11:52:31 +0530

    preloader

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -3,15 +3,20 @@ import "./Card.css";
 import Base from "./Base";
 import Card from "./Card";
 import getProducts from "./helper/coreapicalls";
+import { BounceLoader } from "react-spinners";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const loadAllProduct = () => {
     getProducts().then((data) => {
       if (data.error) {
         setError(data.error);
-      } else setProducts(data);
+      } else {
+        setProducts(data);
+        setLoading(true);
+      }
     });
   };
   console.log(error);
@@ -23,13 +28,17 @@ const Home = () => {
       <h2>All of Gadgets</h2>
       <div className="card__container">
         <div className="card__productContainer">
-          {products.map((product, index) => {
-            return (
-              <div key={index} className="">
-                <Card product={product} />
-              </div>
-            );
-          })}
+          {loading ? (
+            products.map((product, index) => {
+              return (
+                <div key={index} className="">
+                  <Card product={product} />
+                </div>
+              );
+            })
+          ) : (
+            <BounceLoader size={100} />
+          )}
         </div>
       </div>
     </Base>
